c5d8de5646d3388f213432443b52039d80f505cb
alivolley <alicryptovolley@gmail.com>
2023-09-20 10:54:54 +0330

    glowing shadow component, matched for webkit and moz

--- src/components/templates/glowing-shadow/glowing-shadow.style.js
+++ src/components/templates/glowing-shadow/glowing-shadow.style.js
@@ -2,6 +2,8 @@ import styled from '@emotion/styled';
 
 export const GlowingShadowStyle = styled.div(({ color }) => ({
   backgroundColor: color,
-  boxShadow: `0px 0px 300px 300px ${color}`,
   position: 'absolute',
+  boxShadow: `0px 0px 300px 300px ${color}`,
+  WebkitBoxShadow: `0px 0px 300px 300px ${color}`,
+  MozBoxShadow: `0px 0px 300px 300px ${color}`,
 }));
