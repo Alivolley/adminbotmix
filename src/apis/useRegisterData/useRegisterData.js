958c57c63bdd6fa9f86e242daf10337ce560585f
alivolley <alicryptovolley@gmail.com>
2023-10-08 21:47:44 +0330

    authorazation added to login and register

--- src/apis/useRegisterData/useRegisterData.js
+++ src/apis/useRegisterData/useRegisterData.js
@@ -8,6 +8,10 @@ const useRegisterData = () =>
          Cookies.set('botmix_accessToken', res?.data?.access);
          Cookies.set('botmix_refreshToken', res?.data?.refresh);
          Cookies.set('isLogin', true);
+         axiosInstance.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${res?.data?.access}`;
+            return config;
+         });
       })
    );
 
