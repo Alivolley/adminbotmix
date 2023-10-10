dceb2d34dc6291d3443a4eea234711f6beb2d4cc
alivolley <alicryptovolley@gmail.com>
2023-10-10 23:39:38 +0330

    refresh token handled

--- src/configs/axiosInstance.jsx
+++ src/configs/axiosInstance.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-restricted-globals */
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import Cookies from 'js-cookie';
 
 const accessToken = Cookies.get('botmix_accessToken');
-// const refreshToken = Cookies.get('botmix_refreshToken');
+const refreshToken = Cookies.get('botmix_refreshToken');
 
 const axiosInstance = axios.create({
    baseURL: 'https://api.trademix.ir/',
@@ -38,9 +39,31 @@ axiosInstance.interceptors.response.use(
 
       return res;
    },
-   error => {
+   async error => {
       console.log(error);
-      if (error?.response?.data?.message || error?.response?.data?.detail) {
+      const originalReq = error.config;
+
+      if (error?.response?.data?.detail === 'Given token not valid for any token type') {
+         if (refreshToken) {
+            const res = await axiosInstance.post('account/token/refresh/', {
+               refresh: refreshToken,
+            });
+            Cookies.set('botmix_accessToken', res.data.access, { expires: 1 });
+            originalReq.headers.Authorization = `Bearer ${res.data.access}`;
+            return axiosInstance(originalReq);
+         }
+         Cookies.remove('botmix_accessToken');
+         Cookies.remove('botmix_refreshToken');
+         Cookies.remove('isLogin');
+         axiosInstance.interceptors.response.clear();
+         location.href = '/login';
+      } else if (error?.response?.data?.detail === 'Token is invalid or expired') {
+         Cookies.remove('botmix_accessToken');
+         Cookies.remove('botmix_refreshToken');
+         Cookies.remove('isLogin');
+         axiosInstance.interceptors.response.clear();
+         location.href = '/login';
+      } else if (error?.response?.data?.message || error?.response?.data?.detail) {
          toast.error(
             <div className="space-y-3">
                {error?.response?.data?.message && <p className="font-bold">{error?.response?.data?.message}</p>}
