9f94ea15d45fa27244d96f3ed178255a78427b2e
alivolley <alicryptovolley@gmail.com>
2023-10-27 21:24:43 +0330

    pie chart disbled

--- src/components/pages/dashboard/pie-chart-component/pie-chart-component.jsx
+++ src/components/pages/dashboard/pie-chart-component/pie-chart-component.jsx
@@ -1,39 +1,39 @@
-import CanvasJSReact from '@canvasjs/react-charts';
-import { PieChartComponentStyle } from './pie-chart-component.style';
+// import CanvasJSReact from '@canvasjs/react-charts';
+// import { PieChartComponentStyle } from './pie-chart-component.style';
 
-const { CanvasJS } = CanvasJSReact;
-const { CanvasJSChart } = CanvasJSReact;
+// const { CanvasJS } = CanvasJSReact;
+// const { CanvasJSChart } = CanvasJSReact;
 
-function PieChartComponent() {
-   const options = {
-      animationEnabled: true,
-      width: 250,
-      height: 300,
-      backgroundColor: 'transparent',
+// function PieChartComponent() {
+//    const options = {
+//       animationEnabled: true,
+//       width: 250,
+//       height: 300,
+//       backgroundColor: 'transparent',
 
-      data: [
-         {
-            type: 'pie',
-            toolTipContent: '{y}% :<b>{label}</b> ',
-            indexLabelFontSize: 12,
-            indexLabel: '{y}%',
-            indexLabelPlacement: 'inside',
-            indexLabelFontColor: '#ffffff',
-            indexLabelFontFamily: 'vazir',
-            indexLabelDirection: 'ltr',
-            dataPoints: [
-               { y: 75, label: 'کاربر', color: '#3d92c9' },
-               { y: 25, label: 'باتمیکس', color: '#7f9dc3' },
-            ],
-         },
-      ],
-   };
+//       data: [
+//          {
+//             type: 'pie',
+//             toolTipContent: '{y}% :<b>{label}</b> ',
+//             indexLabelFontSize: 12,
+//             indexLabel: '{y}%',
+//             indexLabelPlacement: 'inside',
+//             indexLabelFontColor: '#ffffff',
+//             indexLabelFontFamily: 'vazir',
+//             indexLabelDirection: 'ltr',
+//             dataPoints: [
+//                { y: 75, label: 'کاربر', color: '#3d92c9' },
+//                { y: 25, label: 'باتمیکس', color: '#7f9dc3' },
+//             ],
+//          },
+//       ],
+//    };
 
-   return (
-      <PieChartComponentStyle className="customLg:ml-8 customXl:ml-12">
-         <CanvasJSChart options={options} />
-      </PieChartComponentStyle>
-   );
-}
+//    return (
+//       <PieChartComponentStyle className="customLg:ml-8 customXl:ml-12">
+//          <CanvasJSChart options={options} />
+//       </PieChartComponentStyle>
+//    );
+// }
 
-export default PieChartComponent;
+// export default PieChartComponent;
