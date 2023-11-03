b4d201d2e2c908c425e68fb1d7a85948a0c2a443
alivolley <alicryptovolley@gmail.com>
2023-11-03 22:59:24 +0330

    swiper changed

--- src/components/pages/home/newest-tutorial/newest-tutorial.jsx
+++ src/components/pages/home/newest-tutorial/newest-tutorial.jsx
@@ -1,5 +1,3 @@
-import { useRef, useEffect, useState } from 'react';
-
 // Swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -9,7 +7,7 @@ import 'swiper/css/free-mode';
 import { Pagination, Navigation } from 'swiper/modules';
 
 // MUI
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Grid } from '@mui/material';
 
 // Icons
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
@@ -22,91 +20,73 @@ import { NewestTutorialStyle } from './newest-tutorial.style';
 import TutorialCard from '../../../templates/tutorial-card/tutorial-card';
 
 function NewestTutorial({ detail, loading }) {
-   const swiperRef = useRef(null);
-   const [reload, setReload] = useState(false);
-
-   useEffect(() => {
-      if (swiperRef.current) {
-         const swiperInstance = swiperRef.current.swiper;
-         const slide = detail.length / 2;
-
-         swiperInstance.slideTo(slide);
-      }
-   }, [swiperRef, reload]);
-
-   useEffect(() => {
-      setReload(prev => !prev);
-   }, [swiperRef, loading, detail]);
-
-   console.log(reload);
    return (
-      <NewestTutorialStyle>
+      <NewestTutorialStyle className="mx-auto max-w-[1150px]">
          {detail?.length ? (
             <>
                <p className="text-center font-lalezar text-3xl customMd:text-[50px]">آموزش ها</p>
 
-               <div className="mx-[20px] mt-32 customSm:mx-[-140px]">
+               <div className="mx-[20px] mt-32">
                   {loading ? (
                      <div className="flex items-center justify-center">
                         <CircularProgress />
                      </div>
                   ) : (
-                     <Swiper
-                        spaceBetween={30}
-                        ref={swiperRef}
-                        navigation={{
-                           nextEl: '.swiper-button-next-custom',
-                           prevEl: '.swiper-button-prev-custom',
-                        }}
-                        pagination={{
-                           clickable: true,
-                           renderBullet(index, className) {
-                              return `<span class="w-2 h-2 rounded-full dark:bg-stone-300 ${className}"></span>`;
-                           },
-                        }}
-                        modules={[Pagination, Navigation]}
-                        // eslint-disable-next-line tailwindcss/no-custom-classname
-                        className="mySwiper"
-                        centeredSlides
-                        style={{ paddingBottom: '90px' }}
-                        breakpoints={{
-                           280: {
-                              slidesPerView: 1,
-                           },
-                           450: {
-                              slidesPerView: 2,
-                           },
-                           640: {
-                              slidesPerView: 4,
-                           },
-                           1200: {
-                              slidesPerView: 5,
-                           },
-                        }}
-                     >
-                        {detail?.map(item => (
-                           <SwiperSlide key={item.id}>
-                              <TutorialCard detail={item} />
-                           </SwiperSlide>
-                        ))}
+                     <>
+                        <div className="customSm:hidden">
+                           <Swiper
+                              spaceBetween={30}
+                              navigation={{
+                                 nextEl: '.swiper-button-next-custom',
+                                 prevEl: '.swiper-button-prev-custom',
+                              }}
+                              pagination={{
+                                 clickable: true,
+                                 renderBullet(index, className) {
+                                    return `<span class="w-2 h-2 rounded-full dark:bg-stone-300 ${className}"></span>`;
+                                 },
+                              }}
+                              modules={[Pagination, Navigation]}
+                              // eslint-disable-next-line tailwindcss/no-custom-classname
+                              className="mySwiper"
+                              centeredSlides
+                              style={{ paddingBottom: '90px' }}
+                              slidesPerView={1}
+                           >
+                              {detail?.map(item => (
+                                 <SwiperSlide key={item.id}>
+                                    <TutorialCard detail={item} />
+                                 </SwiperSlide>
+                              ))}
 
-                        <div className="absolute inset-x-0 bottom-10 flex items-center justify-center gap-3 customSm:bottom-0 customSm:gap-64">
-                           <div
-                              // eslint-disable-next-line tailwindcss/no-custom-classname
-                              className="swiper-button-prev-custom z-10 flex cursor-pointer items-center justify-center rounded-full
+                              <div className="absolute inset-x-0 bottom-10 flex items-center justify-center gap-3 customSm:bottom-0 customSm:gap-64">
+                                 <div
+                                    // eslint-disable-next-line tailwindcss/no-custom-classname
+                                    className="swiper-button-prev-custom z-10 flex cursor-pointer items-center justify-center rounded-full
                               bg-primaryBlue text-[28px] text-white customSm:h-14 customSm:w-14 customSm:text-[35px]"
-                           >
-                              <KeyboardArrowRightIcon fontSize="inherit" />
-                           </div>
-                           <div
-                              // eslint-disable-next-line tailwindcss/no-custom-classname
-                              className="swiper-button-next-custom z-10 flex cursor-pointer items-center justify-center rounded-full
+                                 >
+                                    <KeyboardArrowRightIcon fontSize="inherit" />
+                                 </div>
+                                 <div
+                                    // eslint-disable-next-line tailwindcss/no-custom-classname
+                                    className="swiper-button-next-custom z-10 flex cursor-pointer items-center justify-center rounded-full
                               bg-primaryBlue text-[28px] text-white customSm:h-14 customSm:w-14 customSm:text-[35px]"
-                           >
-                              <KeyboardArrowLeftIcon fontSize="inherit" />
-                           </div>
+                                 >
+                                    <KeyboardArrowLeftIcon fontSize="inherit" />
+                                 </div>
+                              </div>
+                           </Swiper>
                         </div>
-                     </Swiper>
+                        <div className="hidden customSm:block">
+                           <Grid container spacing={5} justifyContent="center">
+                              {detail?.map(item => (
+                                 <Grid item xs={4} key={item.id}>
+                                    <TutorialCard detail={item} />
+                                 </Grid>
+                              ))}
+                           </Grid>
+                        </div>
+                     </>
                   )}
                </div>
             </>
